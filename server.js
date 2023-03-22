ccca54de81149d9032b39c73211a97c3fa6cc682
stephen Batelaan <sbatelaan91@gmail.com>
2023-03-21 18:26:44 -0700

    Added functions and else if statements to inject new department and roles into tables

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ let conn = mysql.createConnection({
     user: 'root',
     password: 'ucibootcamp',
     database: 'employee_tracker'
-},
+})
 console.log(`connected to employee_tracker db.`)
-);
+
 
 conn.connect(err => {
     if (err) throw err;
@@ -49,6 +49,97 @@ const empTrack = () => {
              LEFT JOIN employee manager ON employee.manager_id = manager.id`, (err, result) => {
                 console.log('All employees');
                 console.table(result);
+                empTrack();
+            })
+        } else if (answers.prompt === 'Add a department') {
+            inquirer.prompt([
+                {
+                    name: 'department',
+                    type: 'input',
+                    message: 'Enter the department you would like to add:'
+                }
+            ]).then(function(answer) {
+                conn.query('INSERT INTO department (name) VALUES (?)',
+                answer.department,
+                function(err, result) {
+                    if (err) throw err;
+                    console.log(result);
+                    console.log(`Added department ${answer.department}`);
+                    empTrack();
+                })     
+            })
+        } else if (answers.prompt === 'Add a role') {
+            const getDepartmentNames = () => {
+                return new Promise((resolve, reject) => {
+                  conn.query('SELECT name FROM department', (err, results) => {
+                    if (err) {
+                      reject(err);
+                    } else {
+                      // Map the result rows to an array of department names
+                      const departments = results.map(result => result.name);
+                      resolve(departments);
+                    }
+                  });
+                });
+              };
+
+            inquirer.prompt([
+                {
+                    name: 'role',
+                    type: 'input',
+                    message: 'Enter the role you would like to add:'
+                },
+                {
+                    name: 'salary',
+                    type: 'input',
+                    message: 'Enter the salary for this role'
+                },
+                {
+                    name: 'ID',
+                    type: 'list',
+                    message: 'Enter the department ID',
+                    choices: () => getDepartmentNames()
+                }
+            ]).then((answers) => {
+                conn.query('INSERT INTO employee_role (title, salary, department_id VALUES (?, ?, (SELECT id FROM department WHERE name = ?))',
+                [answers.role, answers.salary, answers.department], (err, result) => {
+                    console.log('Added role')
+                    empTrack()
+                })
+            })
+        } else if (answers.prompt = 'Add an employee') {
+            const getRole = () => {
+                return new Promise((resolve, reject) => {
+                  conn.query('SELECT title FROM employee_role', (err, results) => {
+                    if (err) {
+                      reject(err);
+                    } else {
+                      // Map the result rows to an array of department names
+                      const newRole = results.map(result => result.title);
+                      resolve(newRole);
+                    }
+                  });
+                });
+            };
+            inquirer.prompt([
+                {
+                    name: 'firstName',
+                    type: 'input',
+                    message: 'What is the employees first name?'
+                },
+                {
+                    name: 'lastName',
+                    type: 'input',
+                    message: 'What is the employees last name?'
+                },
+                {
+                    name: 'role',
+                    type: 'list',
+                    message: 'What is the employees role?',
+                    choices: () => getRole()
+                }
+            ]).then((answers) => {
+                conn.query('INSERT INTO ')
             })
         }
     })
